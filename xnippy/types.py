91d4c53310d8f0c61e4fa08d6bdb9e7bad37ed52
SungHo Lee <shlee@unc.edu>
2024-05-03 20:28:54 -0400

    feat(xnippy) initiation of xnippy package

---
+++ xnippy/types.py
@@ -0,0 +1,49 @@
+from typing import NewType, Type, Optional, Union
+from typing import Literal, Tuple, List
+from pathlib import Path
+from .manager import Manager
+from .fetcher import SnippetsFetcher
+from .fetcher.base import Fetcher
+from .snippet.base import Snippet
+from .snippet import PlugInSnippet
+from .snippet import PresetSnippet
+from .snippet import RecipeSnippet
+from .snippet import SpecSnippet
+from .formatter.recipe import Resource    
+
+
+ResourceType = Type[Union[Resource, List[Resource]]]
+
+ConfigManagerType = Type[Manager]
+
+StorageMode = Literal['local', 'global']
+
+FetcherType = Type[Fetcher]
+
+SnippetsFetcherType = Type[SnippetsFetcher]
+
+SnippetType = Type[Snippet]
+
+SnippetPath = NewType[
+    Tuple[Optional(Path), 
+          bool]
+    ]
+
+SnippetMode = Literal[
+    'plugin', 'preset', 'spec', 'recipe'
+    ]
+
+PlugInSnippetType = Type[PlugInSnippet]
+
+PresetSnippetType = Type[PresetSnippet]
+
+RecipeSnippetType = Type[RecipeSnippet]
+
+SpecSnippetType = Type[SpecSnippet]
+
+__all__ = [
+    'ConfigManagerType', 'StorageMode',
+    'FetcherType', 'SnippetsFetcherType', 
+    'SnippetType', 'SnippetPath', 'SnippetMode', 'FileSnippetMode', 'ConfigSnippetMode',
+    'PlugInSnippetType', 'PresetSnippetType', 'RecipeSnippetType', 'SpecSnippetType', 'ConfigSnippetType'
+    ]
